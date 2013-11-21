0f435315f9854071ac0a5c2543002a011ce08929
UnkelTao <wangyue121519@gmail.com>
2013-11-21 19:57:04 +0800

    修改程序算法2

---
+++ src/unkeltao/CanopyCenterMapper.java
@@ -0,0 +1,34 @@
+package com.unkeltao;
+
+import static com.unkeltao.KmeansUtils.*;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class CanopyCenterMapper extends Mapper<Text, Movie, Text, Movie> {
+
+	private Text centerKey = new Text(CENTER_KEY);
+	private List<Movie> centers = new ArrayList<Movie>();
+
+	@Override
+	protected void map(Text key, Movie movie, Context context) throws IOException, InterruptedException {
+		
+		boolean stronglyAssociated = false;
+		for (Movie center : centers) {
+			if (isStronglyAssociatedToCenter(movie, center)) {
+				stronglyAssociated = true;
+			}
+		}
+		if (!stronglyAssociated) {
+			centers.add(movie.getCloneMovie());
+		
+			context.write(centerKey, movie);
+		}
+		else {
+		}
+	}
+
+}
